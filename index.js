047ab4532a3233b7dee9f942e1cfa7b955f3e9cc
eustace douglas <eadouglas@live.co.uk>
2018-09-21 10:49:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array1 = []
+    for (var i = 0; i < 4; i++) {
+    array1.push(musicians[i] + " plays " + instruments[i])
+    }
+  return array1
+}
+
+function johnLennonFacts(facts) {
+  var factString = [];
+  var index = 0;
+  while (facts.length > factString.length) {
+    factString.push(facts[index++] + "!!!")
+  }
+  return factString
+}
